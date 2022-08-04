5426d018ade950be7d2cb6c2e94b7a4b3223bd9b
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-08-04 22:07:15 +0200

    now we show a menu, and hide it when we click play

--- index.js
+++ index.js
@@ -1,41 +1,25 @@
 const restartButton = document.getElementById("restartButton");
-
-window.onload = function () {
-  const canvas = document.querySelector("canvas");
-  canvas.focus();
-
-  Game.init();
-};
+const startButton = document.getElementById("startButton");
 
 restartButton.addEventListener("click", function () {
   location.reload();
 });
 
-// altKey: false
-// bubbles: true
-// cancelBubble: false
-// cancelable: true
-// charCode: 0
-// code: "ArrowDown"
-// composed: true
-// ctrlKey: false
-// currentTarget: null
-// defaultPrevented: false
-// detail: 0
-// eventPhase: 0
-// isComposing: false
-// key: "ArrowDown"
-// keyCode: 40
-// location: 0
-// metaKey: false
-// path: (5) [canvas#myCanvas, body, html, document, Window]
-// repeat: false
-// returnValue: true
-// shiftKey: false
-// sourceCapabilities: InputDeviceCapabilities {firesTouchEvents: false}
-// srcElement: canvas#myCanvas
-// target: canvas#myCanvas
-// timeStamp: 954.3999999985099
-// type: "keydown"
-// view: Window {window: Window, self: Window, document: document, name: '', location: Location, …}
-// which: 40
+startButton.addEventListener("click", function () {
+  document.getElementById("myCanvas").dispatchEvent(
+    new MouseEvent("click", {
+      clientX: 200,
+      clientY: 150,
+      bubbles: true,
+    })
+  );
+
+  let startButtonDiv = document.getElementById("startButton");
+  let canvasDiv = document.getElementById("myCanvas");
+  let startbuttonDiv = document.getElementById("startbuttonDiv");
+
+  startButtonDiv.classList.add("hidden");
+  canvasDiv.classList.remove("backgroundImage");
+  startbuttonDiv.classList.add("hidden");
+  Game.init();
+});
